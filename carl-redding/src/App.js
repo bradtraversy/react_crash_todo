ab4e9bef83eb6c5f3771dd75ff3efec70e16fc6d
Carlos <miklo.concepcion@gmail.com>
2019-08-20 09:47:09 -0500

    first commit

---
+++ carl-redding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        HOLA MI GENTE
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ carl-redding/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>App</h1>
+            </div>
+        );
+    }
+}
+
+export default Todos;
