d0becc3fdbd9abacfde52fcd6223c80ed153ae23
profgrammer <javarep4199@gmail.com>
2019-09-08 19:17:20 +0530

    tick checkboxes of completed todos on load

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -12,11 +12,11 @@ export class TodoItem extends Component {
   }
 
   render() {
-    const { id, title } = this.props.todo;
+    const { id, title, completed } = this.props.todo;
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+          <input checked={completed} type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
           { title }
           <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
         </p>
