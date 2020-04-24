1df3d92a6b162102be29548d6a67c14f3f3b3b1b
ishitb <53562523+ishitb@users.noreply.github.com>
2020-04-24 06:50:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
